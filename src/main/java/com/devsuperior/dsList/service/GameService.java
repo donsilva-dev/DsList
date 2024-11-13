0a19ca534545fb9be6f4f6773b3f8d138d0f9d5e
Douglas nunes <donsilva-dev>
2024-11-13 11:17:44 -0300

    Domain model

--- src/main/java/com/devsuperior/dsList/service/GameService.java
+++ src/main/java/com/devsuperior/dsList/service/GameService.java
@@ -17,7 +17,7 @@ public class GameService {
 
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
-		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		return result.stream().map(game -> new GameMinDTO(game)).toList();
 		
 	}
 }
